0993b16d78a56838fa0f8c6b48a42c8927dacc6e
Ryanrb6190 <91529855+Ryanrb6190@users.noreply.github.com>
2025-12-29 21:22:03 +0000

    Set up for local android testing. Updated style sheet for 2nd page.

---
+++ app/(tabs)/explore.jsx
@@ -0,0 +1,43 @@
+import { View, Text, StyleSheet, ImageBackground } from 'react-native'
+import React from 'react'
+
+import theGiantImg from "@/assets/images/the-giant.jpg"
+
+
+const explore = () => {
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+      source={theGiantImg}
+      resizeMode="cover"
+      style={styles.image}
+      >
+      <Text style={styles.text}>explore</Text>
+      </ImageBackground>
+    </View>
+  )
+}
+
+export default explore
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+  },
+
+  image: {
+    width: "100%",
+    height: "100%",
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center"
+  },
+
+  text: {
+    color: "white",
+    fontSize: 42,
+    fontWeight: "bold",
+    textAlign: "center"
+  }
+})
